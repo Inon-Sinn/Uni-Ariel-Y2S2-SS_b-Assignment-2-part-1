b7b51fae6f29b1d7e8605c1ef9fb8d4a2ff1f9d6
Inon Sinn <inon.sinn@gmail.com>
2022-03-23 18:42:06 +0200

    Bad input tests

--- Test.cpp
+++ Test.cpp
@@ -6,8 +6,46 @@
 
 #include "Notebook.hpp"
 #include "Direction.hpp"
+#include "doctest.h"
 #include <iostream>
 #include <stdexcept>
 #include <string>
 #include <algorithm>
 
+using namespace ariel;
+
+// tests for cases where we pass the limit of 100 char in a row,
+TEST_CASE("Bad input"){
+    ariel::Notebook testBook;
+
+    // write after the end of the line
+    CHECK_THROWS(testBook.write(0,0,100, Direction::Vertical,"test"));
+    CHECK_THROWS(testBook.write(0,0,100000, Direction::Vertical,"test"));
+    // read after the end of the line
+    CHECK_THROWS(testBook.read(0,0,100, Direction::Vertical,1));
+    CHECK_THROWS(testBook.read(0,0,100000, Direction::Vertical,1));
+    CHECK_THROWS(testBook.read(0,0,100, Direction::Vertical,0));
+    CHECK_THROWS(testBook.read(0,0,100000, Direction::Vertical,0));
+    // erase after the end of the line
+    CHECK_THROWS(testBook.erase(0,0,100, Direction::Vertical,1));
+    CHECK_THROWS(testBook.erase(0,0,100000, Direction::Vertical,1));
+    CHECK_THROWS(testBook.erase(0,0,100, Direction::Vertical,0));
+    CHECK_THROWS(testBook.erase(0,0,100000, Direction::Vertical,0));
+};
+
+TEST_CASE("Test for 'write' and 'read'"){
+    ariel::Notebook testBook;
+    
+    // Good Input
+    // CHECK_EQ(testBook.write)
+};
+
+TEST_CASE("Function: read"){
+    ariel::Notebook testBook;
+
+};
+
+TEST_CASE("Function: erase"){
+    ariel::Notebook testBook;
+
+};
